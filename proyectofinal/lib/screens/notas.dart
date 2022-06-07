6d25607612f58958f9da7bd91323c8549452db6e
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-06-07 00:53:15 -0700

    detalles finales

--- proyectofinal/lib/screens/notas.dart
+++ proyectofinal/lib/screens/notas.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Notas extends StatelessWidget {
   const Notas({Key? key}) : super(key: key);
@@ -6,65 +9,66 @@ class Notas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Scaffold(
-        appBar: AppBar(title: const Text("Notas Alimenticios")),
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://i.pinimg.com/564x/01/db/4a/01db4af85eb200658ad00b7a83d5be02.jpg'),
-              fit: BoxFit.cover,
-            ),
+        child: Scaffold(
+      appBar: AppBar(title: const Text("Notas Alimenticios")),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://img.freepik.com/free-vector/fruits-background_34401-404.jpg'),
+            fit: BoxFit.cover,
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            Card(
+              child: InkWell(
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(0),
+                      image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              'https://i.pinimg.com/736x/51/52/8f/51528f475951f4c110096da4e6d66883.jpg'))),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Image(
-                        image: AssetImage("assets/img/agua.png"),
-                        width: 80,
-                        height: 90),
-                    Text(
-                        "1. Toma suficiente agua\n\nTodas las células del cuerpo necesitan de\n agua para funcionar. Una adecuada\n hidratación te puede ayudar a mejorar\n tu digestión, la función de tus riñones\n y lucir una piel más hidratada")
-                  ],
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Text(
-                        "2. Actividad física\n\n La OMS recomienda practicar 150min\n de ejercicio físico cada semana. Es decir,\n 30 minutos diarios por 5 días a la semana."),
-                    Image(
-                        image: AssetImage("assets/img/fis.png"),
-                        width: 80,
-                        height: 90),
-                  ],
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Image(
-                        image: AssetImage("assets/img/reduce.png"),
-                        width: 90,
-                        height: 90),
-                    Text(
-                        "3. Reduce o evita el consumo de bebidas\n alcohólicas\n\nEl consumo excesivo de bebidas alcohólicas\n es dañino para tu salud. La  ingesta crónica\n puede ocasionar daños al hígado, riñones,\n corazón y páncreas.\n")
-                  ],
+              children: const [
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Text(
+                      " \n Un naranja al día protege tus ojos.\nLas personas que comen a diario naranjas tienen un \n60% menos riesgo de degeneración macular asociada \na la edad (DMAE).\n"),
                 ),
               ],
             ),
-          ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Text(
+                      " \n Comer despacio te ayudará a ser más feliz\nAdemás de ayudarte a hacer mejor las digestiones,\n también te ayudará a combatir el estrés y por lo tanto\n a tener una vida más feliz.\n"),
+                ),
+              ],
+            ),
+            const Text(
+              "\n\nEsta página se encuntra en servicio, para brinarte un mejor servicio y calidad de información",
+              textAlign: TextAlign.justify,
+            ),
+          ]),
         ),
       ),
-    );
+    ));
   }
 }
